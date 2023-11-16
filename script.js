650d93eb0bd9b54f4197ac1eac2fa8ffdb91fba5
Tiffanie23 <150520912+Tiffanie23@users.noreply.github.com>
2023-11-16 11:24:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+
+    var typed = new Typed(".typing", {
+        strings: ["A STUDENT", "A STUDENT","A DAUGHTER", "A MOTHER", "A PARTNER", "A FRIEND"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-1", {
+        strings: ["Is my Family's Name","is only an overview of my family's information. It identify by these names."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Is my Family's Name","Thank you for visiting, and I hope you enjoy your stay!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+  
+});
